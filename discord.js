186415cd37008d59ad03b98c53347f5a832f8622
John <CWing22@users.noreply.github.com>
2019-03-01 17:54:22 -0600

    Create discord.js

---
+++ discord.js
@@ -0,0 +1,14 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+ 
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+ 
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('pong');
+  }
+});
+ 
+client.login('NTQ2MTEzOTkzMDU2NTgzNjgw.D1tIZg.D4IrTwURTAoxMXLgy_QaXRXd9Nw');

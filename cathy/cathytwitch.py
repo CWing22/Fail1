56f552cebdb027a86bd38599871e07910766c0a3
John <CWing22@users.noreply.github.com>
2019-03-01 05:41:58 -0600

    Update cathytwitch.py

--- cathy/cathytwitch.py
+++ cathy/cathytwitch.py
@@ -4,9 +4,9 @@ import requests
 import aiml
 import irc.bot
 
-USERNAME = "crack_azz" # Substitute your bot's username
+USERNAME = "FoxyVamp" # Substitute your bot's username
 TOKEN = "oauth:obejk6vxzumkae8hhgxpujowx1px3u" # OAUTH token (Get one here: https://twitchapps.com/tmi/)
-CHANNEL = "#stilettoninja" # Twitch channel to join
+CHANNEL = "Foxy_Fury" # Twitch channel to join
 
 STARTUP_FILE = "std-startup.xml"
 BOT_PREFIX = ('?', '!')
@@ -23,7 +23,7 @@ class ChattyCathy(irc.bot.SingleServerIRCBot):
 
         # Create IRC bot connection
         server = 'irc.chat.twitch.tv'
-        port = 443
+        port = 667
         print("Connecting to {} on port {}...".format(server, port))
         irc.bot.SingleServerIRCBot.__init__(self, [(server, port, token)], username, channel)
 

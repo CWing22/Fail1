f762ae212beea02b6a279c6462ce82ba3e3d0010
John <CWing22@users.noreply.github.com>
2019-02-28 23:35:59 -0600

    Update __init__.py

--- cathy/__init__.py
+++ cathy/__init__.py
@@ -1,1 +1,1 @@
-def __init__(self, foxyvamp,NTQ2MTEzOTkzMDU2NTgzNjgw.D1pMgA.XgZMwPpwYK6FQN1x3OJ6U-KLcrM):
+channel_room = foxyvamp, token = NTQ2MTEzOTkzMDU2NTgzNjgw.D1pMgA.XgZMwPpwYK6FQN1x3OJ6U-KLcrM
